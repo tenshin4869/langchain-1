7427ec8213c1af72aab7554e32ebf5074f2213b5
ten <ten20031017-sty@ezweb.ne.jp>
2024-09-08 10:32:14 +0900

    memory機能を実装

---
+++ src/gradio_app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+import time
+from dotenv import load_dotenv
+from chatbot_engine import chat
+from langchain.memory import ChatMessageHistory
+
+
+
+def respond(message, chat_history):
+        history = ChatMessageHistory()
+        for [user_message, ai_message] in chat_history:
+             history.add_user_massage(user_message)
+             history.add_ai_message(ai_message)
+
+        bot_message = chat(message, history)
+        chat_history.append((message, bot_message))
+        time.sleep(2)
+        return "", chat_history
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+if __name__ == "__main__":
+    load_dotenv()
+    demo.launch()
